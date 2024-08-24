d60023ee2055200eab7136e6685f77f0123e9f85
yuktha-c <yuktha.c04@gmail.com>
2024-08-24 13:44:36 +0530

    gallery done

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import './Gallery.css'
+import './Gallery.css'; 
 import gimg1 from '../../img1.jpeg'
 import gimg2 from '../../gimg2.jpeg'
+import { height } from '@fortawesome/free-solid-svg-icons/fa0';
 
 const Gallery = () => {
   return (
-    <div className='gallery'>
-      <div className="g">
-        <a href="" target="_blank" rel="noopener noreferrer">1
-          <img
-            src={gimg1}
-            alt="Image 1"
-          />
-        </a>
-        <a href="" target="_blank" rel="noopener noreferrer">2
-          <img
-            src={gimg2}
-            alt="Image 2"
-          />
-        </a>
-        
-        {/* <a href="https://unsplash.com/photos/GfAC-LoRqMU" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://images.unsplash.com/photo-1553870587-ffe7752a5b1f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1330&q=80"
-            alt="Image 4"
-          />
-        </a> */}
-      </div>
-      <article>
-        <h1 className='blog-headings '>Step into our world!</h1>
+    <div className='gallery-page'>
+    <div className='blog-content'>
+      <h2  style={{textAlign:"center"}}  className='blog-headings'>STEP INTO OUR WORLD!</h2>
+      <div >
         <p>
+        At Adwitiya Financial Services, we believe in the power of financial freedom. Our gallery reflects our journey, our clients' successes, and the moments that define our commitment to helping individuals and businesses achieve their financial goals.</p>
+      </div>
+        {/* <div className='gallery-page'>
+      <div className="responsive">
+        <div className="gallery">
+          <a >
+            <img src={gimg1} alt="Cinque Terre" width="600" height="400" />
+          </a>
+          
+        </div>
+      </div>
+
+      <div className="responsive">
+        <div className="gallery">
+          <a >
+            <img src={gimg2} alt="Forest" width="600" height="400" />
+          </a>
+         
+        </div>
+      </div>
+      </div> */}
+      <div class="responsive-container-block teamcard-container">
+    <div class="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div class="team-card">
+        <div class="team-img-wrapper">
+          <img class="team-img" src={gimg1} style={{height:"auto"}}/>
+        </div>
         
-At Adwitiya Financial Services, we believe in the power of financial freedom. Our gallery reflects our journey, our clients' successes, and the moments that define our commitment to helping individuals and businesses achieve their financial goals.
-        </p>
-      </article>
+      </div>
+    </div>
+    <div class="responsive-cell-block wk-desk-3 wk-mobile-12 wk-tab-4 wk-ipadp-4 team-card-container">
+      <div class="team-card">
+        <div class="team-img-wrapper">
+          <img class="team-img" src={gimg2} style={{height:"auto"}}/>
+        </div>
+        
+      </div>
+    </div>
+    
+  </div>
+    
+    </div>
     </div>
   );
 };
